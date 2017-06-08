6191d84d39ef88f21e6a09425e3ccbd53b159a7d
zhuanghuan.zhou <zh.zhou@outlook.com>
2017-06-09 01:17:46 +0800

    first commit

---
+++ src/main/java/my/demo/activiti/ProcessEngineUtils.java
@@ -0,0 +1,13 @@
+package my.demo.activiti;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+
+/**
+ * Created by zhzho on 2017/6/7.
+ */
+public class ProcessEngineUtils {
+    public static void main(String[] args) {
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+    }
+}
